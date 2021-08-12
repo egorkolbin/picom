98beac7bfaf08105497325702a801e24c1f8e5b6
Anya <sobina.ania@yandex.ru>
2021-08-13 01:34:49 +0500

    исправлены проблемы со шрифтами

---
+++ script.js
@@ -0,0 +1,9 @@
+const authorMenuLink = document.querySelector('.menu_author');
+const classicMenuLink = document.querySelector('.menu_classic');
+const dessertMenuLink = document.querySelector('.menu_dessert');
+
+const menuLinks = document.querySelectorAll('.content-menu_item');
+
+const authorMenuSlide = document.querySelector('.content-menu_author');
+const classicMenuSlide = document.querySelector('.content-menu_classic');
+const dessertMenuSlide = document.querySelector('.content-menu_dessert');
