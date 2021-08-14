3a985e2f56d4b6a23173753a0db53b1f069fcc0f
An-nett <80539880+An-nett@users.noreply.github.com>
2021-08-15 02:54:49 +0500

    Update script.js
    Слайдер спрятан для просмотра карты

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
-import 'slick-carousel';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// import 'slick-carousel';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
 
-$('.about-slides').slick({ accessibility: true, arrows: false });
+// $('.about-slides').slick({ accessibility: true, arrows: false });
 
-$('.menu-slides').slick({
-  accessibility: true,
-  arrows: false,
-  adaptiveHeight: true,
-});
+// $('.menu-slides').slick({
+//   accessibility: true,
+//   arrows: false,
+//   adaptiveHeight: true,
+// });
 
-$('.about_menu-item').on('click', function (e) {
-  changeWhenClicking(e, '.about_menu-item', '.about-slides');
-});
+// $('.about_menu-item').on('click', function (e) {
+//   changeWhenClicking(e, '.about_menu-item', '.about-slides');
+// });
 
-$('.about-slides').on('afterChange', function (e) {
-  changeWhenSwiping(e, '.about_menu-item');
-});
+// $('.about-slides').on('afterChange', function (e) {
+//   changeWhenSwiping(e, '.about_menu-item');
+// });
 
-$('.content-menu_item').on('click', function (e) {
-  changeWhenClicking(e, '.content-menu_item', '.menu-slides');
-});
+// $('.content-menu_item').on('click', function (e) {
+//   changeWhenClicking(e, '.content-menu_item', '.menu-slides');
+// });
 
-$('.menu-slides').on('afterChange', function (e) {
-  changeWhenSwiping(e, '.content-menu_item');
-});
+// $('.menu-slides').on('afterChange', function (e) {
+//   changeWhenSwiping(e, '.content-menu_item');
+// });
 
-function changeWhenClicking(evt, linksList, slidesName) {
-  const $slideNum = $(evt.target).data('nav');
-  const $prevActive = $(`${linksList}.active`);
-  $prevActive.removeClass('active');
-  const $newActive = $(`${linksList}[data-nav="${$slideNum}"]`);
-  $newActive.addClass('active');
-  $(slidesName).slick('slickGoTo', $slideNum);
-}
+// function changeWhenClicking(evt, linksList, slidesName) {
+//   const $slideNum = $(evt.target).data('nav');
+//   const $prevActive = $(`${linksList}.active`);
+//   $prevActive.removeClass('active');
+//   const $newActive = $(`${linksList}[data-nav="${$slideNum}"]`);
+//   $newActive.addClass('active');
+//   $(slidesName).slick('slickGoTo', $slideNum);
+// }
 
-function changeWhenSwiping(evt, linksList) {
-  const $slideNum = $(evt.target).slick('slickCurrentSlide');
-  $(linksList).removeClass('active');
-  const $newActive = $(`${linksList}[data-nav="${$slideNum}"]`);
-  $newActive.addClass('active');
-}
+// function changeWhenSwiping(evt, linksList) {
+//   const $slideNum = $(evt.target).slick('slickCurrentSlide');
+//   $(linksList).removeClass('active');
+//   const $newActive = $(`${linksList}[data-nav="${$slideNum}"]`);
+//   $newActive.addClass('active');
+// }
 
 // Scrolling To Sections
 
